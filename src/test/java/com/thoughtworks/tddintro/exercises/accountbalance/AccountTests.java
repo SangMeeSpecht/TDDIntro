32f931baa52c5681bc6f0f92af89c24c5ae64203
SangMeeSpecht <[01;31m[K"email"[m[K: null>
2016-12-16 00:14:57 -0600

    refactor methods

--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -7,23 +7,24 @@ import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class AccountTests {
+    
     @Test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        Account account = new Account();
+        Account account = new Account(100);
         int newBalance = account.depositMoney(50);
         assertThat(newBalance, is(150));
     }
 
     @Test
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-        Account account = new Account();
+        Account account = new Account(100);
         int newBalance = account.withdrawMoney(50);
         assertThat(newBalance, is(50));
     }
 
     @Test
     public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
-        Account account = new Account();
+        Account account = new Account(50);
         int newBalance = account.withdrawMoney(100);
         assertThat(newBalance, is(50));
     }

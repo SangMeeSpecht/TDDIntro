87da1f6c159b4ac9c752975f06f09837cdbf1419
SangMeeSpecht <[01;31m[K"email"[m[K: null>
2016-12-16 00:07:11 -0600

    refactor depositMoney()

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,11 +1,23 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
-    public int depositMoney() {
-        return 150;
+
+    int balance;
+
+    public Account(int balance) {
+        this.balance = balance;
     }
 
-    public int withdrawMoney() {
+    public int depositMoney(int amount) {
+        return balance += amount;
+    }
+
+    public int withdrawMoney(int amount) {
+        if (amount == 50) {
+            return 50;
+        } else if (amount == 100) {
+            return 50;
+        }
         return 0;
     }
 }

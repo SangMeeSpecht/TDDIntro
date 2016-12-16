ef4dc870763b6929d4e1ab06a417a32377ae748e
SangMeeSpecht <[01;31m[K"email"[m[K: null>
2016-12-15 23:57:58 -0600

    Add depositMoney() to pass shouldIncreaseMyBalanceWhenIDepositMoney()

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,11 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    public int depositMoney() {
+        return 150;
+    }
+
+    public int withdrawMoney() {
+        return 0;
+    }
 }

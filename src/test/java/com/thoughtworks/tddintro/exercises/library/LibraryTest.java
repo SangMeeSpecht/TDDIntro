ad64300bfe4a8ef5982d38539014b40a37a67db6
SangMeeSpecht <[01;31m[K"email"[m[K: null>
2016-12-16 20:24:25 -0600

    Add to shouldDeleteBookFromCollectionWhenRemovedByUser() and pass test

--- src/test/java/com/thoughtworks/tddintro/exercises/library/LibraryTest.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/library/LibraryTest.java
@@ -65,10 +65,9 @@ public class LibraryTest {
     }
 
     @Test
-    @Ignore // Remove each @Ignore and implement test
     public void shouldDeleteBookFromCollectionWhenRemovedByUser() throws IOException {
         // Add when/thenReturn here
-
+        when(bufferedReader.readLine()).thenReturn("The Two Towers");
         books.add("The Two Towers");
         library.removeBook();
 
